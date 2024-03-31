6f45dbeb120f3edede7d4be438f5802ad65d0736
sautsokreach <sautsokreach@gmail.com>
2024-03-31 22:22:36 +0700

    test

--- api/routes/Class.routes.js
+++
@@ -1,9 +0,0 @@
-import { getAllCLass, getOneClass } from "../controller/Class.controller.js";
-
-//const { validateToken } = require("../controller/auth.constroller");
-
-const classes = (app) => {
-  app.get("/api/classes", getAllCLass);
-  app.get("/api/class/:id", getOneClass);
-};
-export default classes;
