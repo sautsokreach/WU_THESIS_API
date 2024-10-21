884a52d34f5831be799518d4a4a8fbe5fcfb304d
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2024-10-21 17:14:12 +0700

    Update db.config.js

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -7,7 +7,7 @@ const db = new Pool({
   user: "sautsokreach",
   host: "ep-rough-rice-922718.ap-southeast-1.aws.neon.tech",
   database: "university_schedule",
-  password: "sODHvLXo1UA5",
+  password: "lP2dXvp6Dbqi",
   port: 5432,
   ssl: {
     rejectUnauthorized: false, // You may need to set this to true in a production environment with a valid certificate
